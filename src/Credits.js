3b0ccd33a6e1df211e86be7d7d8bd05f40a2c1aa
wyf76 <yufanweng76@gmail.com>
2025-03-19 14:38:28 -0700

    updated credits

--- src/Credits.js
+++ src/Credits.js
@@ -8,7 +8,7 @@ class Credits extends Phaser.Scene {
         this.add.text(100, 180, 'Game Design & Programming: Yufan Weng', { fontSize: '20px', fill: '#fff' });
         this.add.text(100, 220, 'Assets: Kenney.nl, OpenGameArt.org', { fontSize: '20px', fill: '#fff' });
         this.add.text(100, 260, 'Music: free sound, https://freesound.org/', {fontSize: '20px', fill: '#fff'});
-        this.add.text(100, 290, 'Press SPACE to Return to Main Menu', { fontSize: '20px', fill: '#fff' });
+        this.add.text(100, 300, 'Press SPACE to Return to Main Menu', { fontSize: '20px', fill: '#fff' });
 
         this.input.keyboard.on('keydown-SPACE', () => {
             this.scene.start('MainMenu');
